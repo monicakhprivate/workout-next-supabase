bbead2d40704525d2fbb6f14b5538b056719a2ea
Monica <monicakhprivate@hotmail.com>
2022-08-18 23:47:12 +0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+// _app.js
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return (
+   <div>
+     <Navbar/>
+     <Component {...pageProps}/>
+     <Footer />
+   </div>
+ );
 }
 
-export default MyApp
+export default MyApp;
---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase.js
+//https://blog.logrocket.com/build-full-stack-app-next-js-supabase/
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
